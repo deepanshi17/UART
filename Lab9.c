d7e76db9de2c2bf28afca03a25be335dfafb70be
deepanshi17 <43077603+deepanshi17@users.noreply.github.com>
2019-04-20 16:45:05 -0500

    Wrote all transmitter software
    I haven't debugged this yet

--- Lab9.c
+++ Lab9.c
@@ -40,11 +40,34 @@ void EnableInterrupts(void);  // Enable interrupts
 #define PF3       (*((volatile uint32_t *)0x40025020))
 uint32_t Data;      // 12-bit ADC
 uint32_t Position;  // 32-bit fixed-point 0.001 cm
+unsigned char signal[8];
+unsigned char data[8];
+unsigned int adc_data=0;
+unsigned char dec=0;
+unsigned int TxCounter=0;
+int state=0;
+char packetptr;
 
+void SysTick_Init(void){
+	NVIC_ST_CTRL_R = 0; 						
+	NVIC_ST_RELOAD_R = 1333333; 
+	NVIC_ST_CURRENT_R = 0; 
+	NVIC_SYS_PRI3_R = (NVIC_SYS_PRI3_R & 0x00FFFFFF) | 0x20000000;		//is this necessary?
+	NVIC_ST_CTRL_R = 0x07; 
+}
 
 // Initialize Port F so PF1, PF2 and PF3 are heartbeats
 void PortF_Init(void){
 // Intialize PortF for hearbeat
+	volatile int delay=0;
+	SYSCTL_RCGCGPIO_R &= ~0x20;		// enable port F clock
+	SYSCTL_RCGCGPIO_R |= 0x20;
+	delay=0;											// wait for clock to stabilize
+	GPIO_PORTF_DIR_R &= ~0x0E;		// set PF1 - PF3 as output
+	GPIO_PORTF_DIR_R |= 0x0E;
+	GPIO_PORTF_AFSEL_R &= ~0x0E;	// disable alternate function for PF1 - PF3
+	GPIO_PORTF_DEN_R &= ~0x0E;		// enable GPIO for PF1 - PF3
+	GPIO_PORTF_DEN_R |= 0x0E;
 }
 
 
@@ -89,11 +112,11 @@ uint32_t Convert(uint32_t input){
 // Sender sends using SysTick Interrupt
 // Receiver receives using RX
 int main(void){ 
-  
   PLL_Init(Bus80MHz);     // Bus clock is 80 MHz 
   ST7735_InitR(INITR_REDTAB);
   ADC_Init();    // initialize to sample ADC
   PortF_Init();
+	SysTick_Init();
   Uart_Init();       // initialize UART
   ST7735_SetCursor(0,0);
   LCD_OutFix(0);
@@ -102,6 +125,41 @@ int main(void){
   EnableInterrupts();
   while(1){
     //--UUU--Complete this  - see lab manual
+		while(state == 0){
+		}																	// wait until adc mailbox is full
+		state = 0;												// clear state of mailbox
+		ST7735_SetCursor(0,0);						// set cursor to left corner
+		do{
+			Fifo_Get(&packetptr);
+			data[0] = packetptr; 								// waits until fifo isn't empty
+		}while (data[0] == 0x00);
+		
+		for(int j=1; j < 8; j++){								// place fifo data into output array
+			Fifo_Get(&packetptr);
+			data[j] = packetptr;
+		}
+		
+		if((data[0] == 0x02) && (data[7] == 0x03)){
+			data[0] = 0;											// reset packet start char
+			data[7] = 0;											// reset packet end char
+			
+			for(int i=1; i < 6; i++){
+				char place = 0;							// place holding character for debugging purposes
+				place = data[i];
+				ST7735_OutChar(place);				// output character to lcd
+				data[i] = 0;										// reset index in output array
+			}
+			
+			ST7735_SetCursor(6,0);					// set cursor to end
+			ST7735_OutString(" cm.");				// print " cm."
+			
+		}
+		
+		else{
+			for(int x=0;x < 8; x++){								// reset indexes in output array
+				data[x] = 0;
+			}
+		}
   }
 }
 
@@ -109,6 +167,31 @@ int main(void){
 */
 void SysTick_Handler(void){ // every 20 ms
  //Sample ADC, convert to distance, create 8-byte message, send message out UART1
-
+	int i=0;
+	PF2 ^= 0x04;      // Heartbeat
+	adc_data = ADC_In();
+	state=1;
+	PF2 ^= 0x04;      // Heartbeat
+	adc_data = Convert(adc_data);
+	signal[0] = 0x02;					// packet start
+	dec = adc_data / 1000;
+	adc_data %= 1000;
+	signal[1] = dec + 0x30;		// ASCII char of first number
+	signal[2] = 0x2E;					// decimal point
+	dec = adc_data / 100;
+	adc_data %= 100;
+	signal[3] = dec + 0x30;		// ASCII char of second number
+	dec = adc_data / 10;
+	adc_data %= 10;
+	signal[4] = dec + 0x30;		// ASCII char of third number
+	signal[5] = adc_data + 0x30;		// ASCII char of last number
+	signal[6] = 0x0D;					// ASCII char of carriage return
+	signal[7] = 0x03;					// packet end
+	for(i=0;i < 8; i++){
+		Uart_OutChar(signal[i]);	// output packet using UART
+	}
+	
+	TxCounter++; 								// increment transmit counter
+	PF2 ^= 0xFF;								// toggle heartbeat	
 }
 
--- Uart.c
+++ Uart.c
@@ -20,7 +20,6 @@
 #include "Fifo.h"
 #include "Uart.h"
 #include "../inc/tm4c123gh6pm.h"
-
 uint32_t DataLost; 
 // Initialize UART1
 // Baud rate is 115200 bits/sec
@@ -28,12 +27,36 @@ uint32_t DataLost;
 // Write UART1_Handler
 void Uart_Init(void){
    // --UUU-- complete with your code
-	SYSCTL_RCGCUART_R |= 0x0002;
-	SYSCTL_RCGCGPIO_R |= 0X0004;
-	UART1_CTL_R &= ~0x0002;
-	UART1_IBRD_R = 43;
+	volatile int delay=0;
+	SYSCTL_RCGCUART_R |= 0x0002;	//start UART clock
+	delay=0;
+	delay=0;
+	
+	SYSCTL_RCGCGPIO_R |= 0X0004;	//start Port C clock
+	delay=0;
+	delay=0;
+	
+	Fifo_Init();
+	
+	UART1_CTL_R &= ~0x0001;				//disable UART
+	UART1_IBRD_R = 43;						//baud rate is 115200
 	UART1_FBRD_R = 26;
-	UART1_LCRH_R = 0x0070;
+	UART1_LCRH_R = 0x0070;				//8-bit word length, enable FIFO
+	UART1_CTL_R = 0x0301;					//enable TXE, RXE, and UART
+	
+	
+	GPIO_PORTC_PCTL_R = (GPIO_PORTC_PCTL_R&0xFF00FFFF)+0x00220000;
+	GPIO_PORTC_AFSEL_R |= 0x30;		//alt function 
+	GPIO_PORTC_DEN_R |= 0x30;			//digital analog PC4 and PC5
+	GPIO_PORTC_AMSEL_R &= ~0x30;	//no analog on PC4 and PC5
+	
+	
+	UART1_IM_R |= 0x10;
+	UART1_IFLS_R &= ~0x38;
+	UART1_IFLS_R |= 0x10;
+	NVIC_PRI1_R &= 0xFF00FFFF;		// UART0 = priority 3
+	NVIC_PRI1_R	|= 0x00800000; 		
+	NVIC_EN0_R |= 0x40; 					// enable interrupt 6 in NVIC
 }
 
 // input ASCII character from UART
@@ -60,6 +83,9 @@ void UART1_InMessage(char *bufPt){
 // Transmitter is busywait
 void Uart_OutChar(char data){
   // --UUU-- complete with your code
+	while((UART1_FR_R & 0x0020)){	// wait until TXFF is 0
+	}	
+	UART1_DR_R = data;	
 }
 
 // hardware RX FIFO goes from 7 to 8 or more items
